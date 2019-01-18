4d55d293f73f222a9e9230f66c5d8f00b4b0e11d
Rangi <aravindyadav.rangi@abbott.com>
2019-01-18 15:19:01 +0100

    Customer support module initial commit

---
+++ app/code/Asw/Csupport/Block/Csupport.php
@@ -0,0 +1,10 @@
+<?php
+namespace Asw\Csupport\Block;
+ 
+class Csupport extends \Magento\Framework\View\Element\Template
+{
+    public function getHeaderTxt()
+    {
+        return 'Customer Support Form';
+    }
+}
